311f3374a1d3d7d051eb2df17ebfec075bac8bbd
lklikelk <lklikelk@users.noreply.github.com>
2020-04-18 10:45:03 +0800

    第四部分：CSS 样式

--- src/com/like/MainApp.java
+++ src/com/like/MainApp.java
@@ -20,6 +20,7 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Modality;
@@ -37,6 +38,7 @@ public class MainApp extends Application {
      * The data as an observable list of Persons.
      */
     private ObservableList<Person> personData = FXCollections.observableArrayList();
+
 
     /**
      * Constructor
@@ -68,7 +70,8 @@ public class MainApp extends Application {
     public void start(Stage primaryStage) {
         this.primaryStage = primaryStage;
         this.primaryStage.setTitle("AddressApp");
-
+        // Set the application icon.
+        this.primaryStage.getIcons().add(new Image("file:resources/images/Cook_Book.png"));
         initRootLayout();
 
         showPersonOverview();
@@ -150,7 +153,8 @@ public class MainApp extends Application {
             dialogStage.initOwner(primaryStage);
             Scene scene = new Scene(page);
             dialogStage.setScene(scene);
-
+            // Set the application icon.
+            dialogStage.getIcons().add(new Image("file:resources/images/address_book_32.png"));
             // Set the person into the controller.
             PersonEditDialogController controller = loader.getController();
             controller.setDialogStage(dialogStage);
